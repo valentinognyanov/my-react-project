f59f4b1ef1d331dbc296a6f0ac6eacbab7c4eb2c
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-03-23 20:42:29 +0200

    update movie service

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,22 +1,45 @@
-import * as request from './requester';
+import { requestFactory } from './requester';
 
-const baseUrl = 'http://localhost:3030/jsonstore/movies';
+const baseUrl = 'http://localhost:3030/data/movies';
 
-export const getAll = async () => {
-    const result = await request.get(baseUrl);
-    const movies = Object.values(result);
-    
-    return movies;
-};
+export const movieServiceFactory = (token) => {
+    const request = requestFactory(token);
 
-export const getOne = async (movieId) => {
-    const result = await request.get(`${baseUrl}/${movieId}`);
+    const getAll = async () => {
+        const result = await request.get(baseUrl);
+        const movies = Object.values(result);
 
-    return result;
-};
+        return movies;
+    };
 
-export const create = async (movieData) => {
-    const result = await request.post(baseUrl, movieData);
-    
-    return result;
+    const getOne = async (movieId) => {
+        const result = await request.get(`${baseUrl}/${movieId}`);
+
+        return result;
+    };
+
+    const create = async (movieData) => {
+        const result = await request.post(baseUrl, movieData);
+
+        return result;
+    };
+
+    // const addComment = async (movieId, data) => {
+    //     const result = await request.post(`${baseUrl}/${movieId}/comments`, data);
+
+    //     return result;
+    // };
+
+    const edit = async (movieId, data) => request.put(`${baseUrl}/${movieId}`, data);
+
+    const del = (movieId) => request.del(`${baseUrl}/${movieId}`);
+
+    return {
+        getAll,
+        getOne,
+        create,
+        edit,
+        delete: del,
+        // addComment
+    };
 };
