ab53ff834134d97251cbc4e4aa50c071658b95b8
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-03-23 20:42:55 +0200

    update requester

--- src/services/requester.js
+++ src/services/requester.js
@@ -1,4 +1,4 @@
-const request = async (method, url, data) => {
+const request = async (method, token, url, data) => {
     const options = {};
 
     if (method !== 'GET') {
@@ -10,6 +10,12 @@ const request = async (method, url, data) => {
             };
             options.body = JSON.stringify(data);
         }
+    }
+    if(token) {
+        options.headers = {
+            ...options.headers,
+            'X-Authorization': token
+        } 
     }
     const response = await fetch(url, options);
 
@@ -24,8 +30,12 @@ const request = async (method, url, data) => {
     return result;
 };
 
-export const get = request.bind(null, 'GET');
-export const post = request.bind(null, 'POST');
-export const put = request.bind(null, 'PUT');
-export const patch = request.bind(null, 'PATCH');
-export const del = request.bind(null, 'DELETE');
+export const requestFactory = (token) => {
+    return {
+        get: request.bind(null, 'GET', token),
+        post: request.bind(null, 'POST', token),
+        put: request.bind(null, 'PUT', token),
+        patch: request.bind(null, 'PATCH', token),
+        del: request.bind(null, 'DELETE', token),
+    };
+};
