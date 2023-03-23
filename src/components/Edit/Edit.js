3369bc7ca2cf3a807e6c8a36f93be609daa200e3
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-03-23 20:44:01 +0200

    create edit

---
+++ src/components/Edit/Edit.js
@@ -0,0 +1,82 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { useService } from "../../hooks/useService"; 
+import { useForm } from "../../hooks/useForm";
+import { movieServiceFactory } from "../../services/movieService";
+
+export const Edit = ({
+    onMovieEditSubmit,
+}) => {
+    const { movieId } = useParams();
+    const movieService = useService(movieServiceFactory)
+    const { values, changeHandler, onSubmit, changeValues } = useForm({
+        _id: '',
+        title: '',
+        year: '',
+        runtime: '',
+        director: '',
+        actors: '',
+        imageUrl: '',
+        plot: '',
+        genres: [],
+    }, onMovieEditSubmit);
+
+    useEffect(() => {
+        movieService.getOne(movieId)
+            .then(result => {
+                changeValues(result)
+            });
+    }, [movieId]);
+
+    return (
+        <div className='create-div'>
+            <h3>Edit</h3>
+            <div className='create-form'>
+                <form id='create-form' method='POST' onSubmit={onSubmit}>
+                    <input value={values.title} onChange={changeHandler}  type="text" id="title" name="title" placeholder="Title.." />
+
+                    <input value={values.year} onChange={changeHandler} type="number" id="year" name="year" placeholder="Year.." />
+
+                    <input value={values.runtime} onChange={changeHandler} type="number" id="runtime" name="runtime" placeholder="Runtime.." />
+
+                    <input value={values.director} onChange={changeHandler} type="text" id="director" name="director" placeholder="Director.." />
+
+                    <input value={values.actors} onChange={changeHandler} type="text" id="actors" name="actors" placeholder="Actors.." />
+
+                    <input value={values.imageUrl} onChange={changeHandler} type="text" id="imageUrl" name="imageUrl" placeholder="ImageUrl.." />
+
+                    <textarea value={values.plot} onChange={changeHandler}  type="text" id="plot" name="plot" placeholder="Plot.." />
+
+                    <div className='ganres-checkbox'>
+                        <div>
+                            <h5>Genres</h5>
+                            <input type="checkbox" name="genres" id="comedy" /><label htmlFor="comedy">Comedy</label>
+                            <input type="checkbox" name="genres" id="fantasy" /><label htmlFor="fantasy">Fantasy</label>
+                            <input type="checkbox" name="genres" id="crime" /><label htmlFor="crime">Crime</label>
+                            <input type="checkbox" name="genres" id="drama" /><label htmlFor="drama">Drama</label>
+                            <input type="checkbox" name="genres" id="music" /><label htmlFor="music">Music</label>
+                            <input type="checkbox" name="genres" id="adventure" /><label htmlFor="adventure">Adventure</label>
+                            <input type="checkbox" name="genres" id="history" /><label htmlFor="history">History</label>
+                            <input type="checkbox" name="genres" id="thriller" /><label htmlFor="thriller">Thriller</label>
+                            <input type="checkbox" name="genres" id="animation" /><label htmlFor="animation">Animation</label>
+                            <input type="checkbox" name="genres" id="family" /><label htmlFor="family">Family</label>
+                            <input type="checkbox" name="genres" id="mystery" /><label htmlFor="mystery">Mystery</label>
+                            <input type="checkbox" name="genres" id="biography" /><label htmlFor="biography">Biography</label>
+                            <input type="checkbox" name="genres" id="action" /><label htmlFor="action">Action</label>
+                            <input type="checkbox" name="genres" id="filmNoir" /><label htmlFor="filmNoir">Film-Noir</label>
+                            <input type="checkbox" name="genres" id="romance" /><label htmlFor="romance">Romance</label>
+                            <input type="checkbox" name="genres" id="sciFi" /><label htmlFor="sciFi">Sci-Fi</label>
+                            <input type="checkbox" name="genres" id="war" /><label htmlFor="war">War</label>
+                            <input type="checkbox" name="genres" id="western" /><label htmlFor="western">Western</label>
+                            <input type="checkbox" name="genres" id="horror" /><label htmlFor="horror">Horror</label>
+                            <input type="checkbox" name="genres" id="musical" /><label htmlFor="musical">Musical</label>
+                            <input type="checkbox" name="genres" id="sport" /><label htmlFor="sport">Sport</label>
+                        </div>
+                    </div>
+                    <input type="submit" id='submit' value="Submit" />
+                </form>
+            </div>
+        </div>
+    );
+};
