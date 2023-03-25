67cb855715a6e0c932ece8668ed51e8fcac9239d
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-03-25 18:50:02 +0200

    update card.js

--- src/components/Catalog/Card.js
+++ src/components/Catalog/Card.js
@@ -12,7 +12,7 @@ export const Card = ({
             <div className="card"><img src={imageUrl} alt={_id} />
                 <div className="info">
                     <h1>{`${title} (${year})`}</h1>
-                    <p>{genres.join(', ')}</p>
+                    <p>{genres}</p>
                     <Link to={`/details/${_id}`}>Details</Link>
                 </div>
             </div>
