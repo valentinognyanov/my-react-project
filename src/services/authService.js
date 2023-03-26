dd21eeed65ae257b0cc28dc87e4bbe672d2105de
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-03-26 15:01:21 +0300

    update authService

--- src/services/authService.js
+++ src/services/authService.js
@@ -9,5 +9,6 @@ export const authServiceFactory = (token) => {
         login: (data) => request.post(`${baseUrl}/login`, data),
         register: (data) => request.post(`${baseUrl}/register`, data),
         logout: () => request.get(`${baseUrl}/logout`),
+        // getUserById: (userId) => request.get(`${baseUrl}/${userId}`),
     };
 };
