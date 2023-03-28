6d87f65d8dbd4b1a781586bf0699128d5a550041
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-03-28 21:23:25 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
