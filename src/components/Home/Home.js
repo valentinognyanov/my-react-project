99b6c50af9349ce01afef9b32623683ed7fc2fe1
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-04-01 20:42:14 +0300

    update home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,41 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Home.css';
 
 export const Home = () => {
+    const [latestMovies, setLatestMovies] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:3030/data/movies`)
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+                setLatestMovies(result);
+            })
+    }, []);
+
     return (
-        <div className="accordion" id="accordionExample">
-            <div className="accordion-item">
-                <h2 className="accordion-header" id="headingOne">
-                    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        Iva Ognyanova
-                    </button>
-                </h2>
-                <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                    <div className="accordion-body">
-                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                    </div>
-                </div>
+        <section id="welcome-section">
+            <div id="welcome-message">
+                <h2>Welcome to My Movie Library :)</h2>
             </div>
-        </div>
+
+            <div id="home-page">
+                <h1>Last three uploaded movies</h1>
+                {latestMovies.slice(-3).map(movie => (
+                    <div className="wrapper">
+                        <div className="card"><img src={movie.imageUrl} alt={movie._id} />
+                            <div className="info">
+                                <h1 data-testid="title">{`${movie.title} (${movie.year})`}</h1>
+                                <Link to={`/details/${movie._id}`}>Details</Link>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+                {latestMovies.length === 0 && (<p className='no-movies-home'>No movies yet...</p>)}
+            </div>
+        </section>
     );
 };
