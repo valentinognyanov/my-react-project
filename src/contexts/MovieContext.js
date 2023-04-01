e0b47fcd3324314e5d1d3079102270e0fd2c1a41
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-04-01 13:41:21 +0300

    validate create movie inputs

--- src/contexts/MovieContext.js
+++ src/contexts/MovieContext.js
@@ -12,7 +12,7 @@ export const MovieProvider = ({
     const movieService = movieServiceFactory();
     const navigate = useNavigate();
 
-    useEffect(() => {
+    useEffect(async () => {
         movieService.getAll()
             .then(res => {
                 setMovies(res)
@@ -20,6 +20,49 @@ export const MovieProvider = ({
     }, []);
 
     const onCreateMovieSubmit = async (data) => {
+        const {
+            title,
+            year,
+            runtime,
+            director,
+            actors,
+            imageUrl,
+            plot,
+            genres
+        } = data;
+
+        if (title === '') {
+            throw ({ message: 'Title should be filled.' });
+        } else if (title.length <= 2) {
+            throw ({ message: 'Tittle should be at least 3 charachters' });
+        }
+        if (year === '') {
+            throw ({ message: 'Year should be filled.' });
+        }
+        if (runtime === '') {
+            throw ({ message: 'Runtime should be filled.' });
+        }
+        if (director === '') {
+            throw ({ message: 'Director should be filled.' });
+        } else if (director.length < 5) {
+            throw ({ message: 'Director should be at least 5 characters.' });
+        }
+        if (actors === '') {
+            throw ({ message: 'Actors should be filled.' });
+        } else if (actors.length < 5) {
+            throw ({ message: 'Actors should be at least 5 characters.' });
+        }
+        if (imageUrl === '') {
+            throw ({ message: 'Image URL should be filled.' });
+        }
+        if (plot === '') {
+            throw ({ message: 'Plot should be filled.' });
+        } else if (plot.length < 10) {
+            throw ({ message: 'Plot should be at least 10 charachters.' });
+        }
+        // if (genres.length === 0) {
+        //     throw ({ message: 'At least one genre should be chosen.' });
+        // }
         const newMovie = await movieService.create(data);
 
         setMovies(movies => [...movies, newMovie]);
