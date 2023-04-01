ff86cf5cd47af09675773f2087f302dd28761daf
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-04-01 22:06:59 +0300

    update search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react';
 import './Search.css';
 
 export const Search = () => {
+
+    const [input, setInput] = useState('');
+
+    // useEffect(() => {
+
+    // }, '');
+
+    const onChangeSearchHandler = (e) => {
+        e.preventDefault();
+
+        
+    }
+
     return (
         <div className="wrap">
             <div className="search">
-                <input type="text" className="searchTerm" placeholder="Search..." />
+                <input value={input} onChange={onChangeSearchHandler} type="text" className="searchTerm" placeholder="Search..." />
                 <button type="submit" className="searchButton"></button>
             </div>
             <div className="search-result">
