2a3b7b1770eb71d96016edb85b923400a2e9240a
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-04-02 19:05:04 +0300

    update catalog with pagination

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,19 +1,55 @@
-import { useMovieContext } from '../../contexts/MovieContext';
+import { useState } from 'react';
 
+import { useMovieContext } from '../../contexts/MovieContext';
 import { Card } from './Card';
-import './Catalog.css';
+import { Paginate } from '../Pagination/Paginate';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Catalog.css';
 
 export const Catalog = () => {
     const { movies } = useMovieContext();
+    const [currentPage, setCurrentPage] = useState(1);
+    const [moviesPerPage] = useState(12);
+
+    const indexOfLastMovie = currentPage * moviesPerPage;
+    const indexOfFirstMovie = indexOfLastMovie - moviesPerPage;
+    const currentMovie = movies.slice(indexOfFirstMovie, indexOfLastMovie);
+
+    const paginate = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    const previousPage = () => {
+
+        if (currentPage !== 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
+    const nextPage = () => {
+
+        if (currentPage !== Math.ceil(movies.length / moviesPerPage)) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
 
     return (
-        <section className='catalog-body'>
-            {movies.map(x => <Card key={x._id} {...x} />)}
+        <>
+            <section className='catalog-body'>
+                {currentMovie.map(x => <Card key={x._id} {...x} />)}
 
-            {movies.length === 0 && (
-                <h2 className='no-movies'>There are no movies yet...</h2>
-            )}
-        </section>
+                {currentMovie.length === 0 && (
+                    <h2 className='no-movies'>There are no movies yet...</h2>
+                )}
+            </section>
+            <Paginate
+                moviesPerPage={moviesPerPage}
+                totalMovies={movies.length}
+                paginate={paginate}
+                previousPage={previousPage}
+                nextPage={nextPage}
+            />
+        </>
     );
 };
