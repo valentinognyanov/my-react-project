985aef7cf72cf6df85ca62fbb0de09914bf853df
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-04-02 12:30:47 +0300

    update home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,14 +22,14 @@ export const Home = () => {
                 <h1 data-testid="welcome">Welcome to My Movie Library</h1>
             </div>
             <div id="welcome-image">
-                <img data-testId="welcome-img" src="/images/home-page-img.png" />
+                <img data-testid="welcome-img" src="/images/home-page-img.png" alt='home-img' />
             </div>
 
             <div id="home-page">
-                <h2 data-testId="last-five">Last five uploaded movies</h2>
-                {latestMovies.slice(-5).map(movie => <LastFiveMovies key={movie._id} {...movie} />)}
-                {latestMovies.length === 0 && (<p className='no-movies-home'>No movies yet...</p>)}
+                <h2 data-testid="last-five">Last five uploaded movies</h2>
+                {latestMovies?.slice(-5).map(movie => <LastFiveMovies key={movie._id} {...movie} />)}
             </div>
+            {latestMovies.length === 0 && (<p className='no-movies-home'>No movies yet...</p>)}
         </section>
     );
 };
