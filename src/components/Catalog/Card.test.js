57f40acd1898a6aea3b2998b0c409a73c0798880
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-04-07 21:36:13 +0300

    update card component unit testing

--- src/components/Catalog/Card.test.js
+++ src/components/Catalog/Card.test.js
@@ -1,63 +1,73 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { act, render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
 import { Card } from './Card';
 
-test('Correct behavior when click on details button', async () => {
+test('Correct behavior when click on details button.', async () => {
     global.window = { location: { pathname: null } };
 
     const movieId = 'id';
 
-    render(
-        <BrowserRouter>
-            <Card _id={movieId} />
-        </BrowserRouter>
-    );
+    act(() => {
+        render(
+            <BrowserRouter>
+                <Card _id={movieId} />
+            </BrowserRouter>
+        );
+    });
 
-    await userEvent.click(screen.queryByText('Details'));
+    const detailsBtnElement = screen.queryByText('Details');
+
+    await userEvent.click(detailsBtnElement);
 
     expect(global.window.location.pathname).toContain(`/details/${movieId}`);
 });
 
-test('Correct title name display', async () => {
+test('Correct title name display.', async () => {
     const name = 'Name';
     const year = 'year';
 
-    render(
-        <BrowserRouter>
-            <Card title={name} year={year} />
-        </BrowserRouter>
-    );
+    act(() => {
+        render(
+            <BrowserRouter>
+                <Card title={name} year={year} />
+            </BrowserRouter>
+        );
+    });
 
     const title = await screen.getByTestId('title');
 
     expect(title).toHaveTextContent(`${name} (${year})`);
 });
 
-test('Correct genres display', async () => {
+test('Correct genres display.', async () => {
     const genres = [];
 
-    render(
-        <BrowserRouter>
-            <Card genres={genres} />
-        </BrowserRouter>
-    );
+    act(() => {
+        render(
+            <BrowserRouter>
+                <Card genres={genres} />
+            </BrowserRouter>
+        );
+    });
 
     const genresElement = await screen.getByTestId('genres');
 
     expect(genresElement).toHaveTextContent(`${genres}`);
 });
 
-test('Correct image url', async () => {
+test('Correct image url.', async () => {
     const imageUrl = '';
     const id = '';
 
-    render(
-        <BrowserRouter>
-            <Card imageUrl={imageUrl} _id={id} />
-        </BrowserRouter>
-    );
+    act(() => {
+        render(
+            <BrowserRouter>
+                <Card imageUrl={imageUrl} _id={id} />
+            </BrowserRouter>
+        );
+    });
 
     const imageElement = await screen.getByAltText(id);
 
